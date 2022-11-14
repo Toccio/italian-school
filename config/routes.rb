09b9938a6a51cc9041fc7740b8c160219c31cf29
Marcin Niemiec <marcinniemiec87@gmail.com>
2022-11-14 22:33:00 +0100

    Model, cotroller and index.html.erb of Courses has been created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
     root to: 'schools#home'
+
+    resources :courses, only: %i[index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_14_212412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "max_capacity"
+    t.string "level"
+    t.integer "time"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
